0ba5f12e7cc4542d15c6f4df1b6f6c8f32bbdf0d
Ajaco998 <agjacobson7@gmail.com>
2025-08-28 12:38:57 -0500

    inital commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Name: Andrew Jacobson
+ * Date: 2025/8/28
+ * Email: ajacobson14@huskers.unl.edu
+ *
+ * Purpose: Prints my name to the terminal
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NAME "Andrew Jacobson"
+
+int main(int argc, char **argv) {
+
+    printf("%s\n", NAME);
+    return 0;
+}
